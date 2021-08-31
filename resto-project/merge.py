6a9d8680b6eb43ba5ad367bbc89967e4c5951dc6
Eric Blanvillain <b00684319@essec.edu>
2021-08-31 17:21:05 +0200

    fix merge match happening

--- resto-project/merge.py
+++ resto-project/merge.py
@@ -4,17 +4,34 @@ def merge():
 
     db = pd.read_csv('../raw_data/df_feat_d2.csv')
     d16 =pd.read_csv('../raw_data/df_feat_d16.csv')
-    all_events = pd.read_csv('../exo_data/all_events_sport_and_weather.csv')
+    #all_events = pd.read_csv('../exo_data/all_events_sport_and_weather.csv')
+    data_weather = pd.read_csv('../exo_data/data_weather.csv')
+    #data_sports = pd.read_csv('../exo_data/data_sports.csv')
+    data_events = pd.read_csv('../exo_data/data_events.csv')
+    data_vacances = pd.read_csv('../exo_data/data_vacances.csv')
+    psg_matches = pd.read_csv('../exo_data/psg_matches.csv')
+    psg_matches_cl = pd.read_csv('../exo_data/psg_matches_cl.csv')
 
 
     #-------------------------------------------------------
 
     # Merge d2
-    df_d2 = db.merge(all_events, how='left', left_on=["date", "service"], right_on=["date","service"])
+    df_d2 = db.merge(data_weather, how='left', left_on=["date", "service"], right_on=["date","service"])\
+    .merge(data_events, how='left', left_on=["date", "service"], right_on=["date","service"])\
+    .merge(data_vacances, how='left', left_on=["date", "service"], right_on=["date","service"])\
+    .merge(psg_matches, how='left', left_on=["date", "service"], right_on=["date","service"])\
+    .merge(psg_matches_cl, how='left', left_on=["date", "service"], right_on=["date","service"])
+
 
     # Pb avec météo, on drop duplicates
     df_d2 = df_d2.drop_duplicates(subset=['date', 'service'], keep='first')
     df_d2.drop(columns = ['CA_HT','TVA'], inplace= True)
+    df_d2.drop(columns=['Location', 'Home Team', 'Away Team'], inplace=True)
+    df_d2['Match Happening'].replace(to_replace=0.0,value=1,inplace=True)
+    df_d2['Match Happening-CL'].replace(to_replace=0.0,value=1,inplace=True)
+    df_d2['match_happening'] = df_d2['Match Happening'].copy()
+    df_d2['match_happening_cl'] = df_d2['Match Happening-CL'].copy()
+    df_d2.drop(columns=['Match Happening', 'Match Happening-CL'],inplace=True)
 
     # Reset index
     df_d2 = df_d2.reset_index(drop=True)
@@ -24,10 +41,20 @@ def merge():
     # OK pour d2
 
     # Merge d16 ----------------------------------------------
-    df_d16 = d16.merge(all_events, how='left', left_on=["date", "service"], right_on=["date","service"])
+    df_d16 = d16.merge(data_weather, how='left', left_on=["date", "service"], right_on=["date","service"])\
+    .merge(data_events, how='left', left_on=["date", "service"], right_on=["date","service"])\
+    .merge(data_vacances, how='left', left_on=["date", "service"], right_on=["date","service"])\
+    .merge(psg_matches, how='left', left_on=["date", "service"], right_on=["date","service"])\
+    .merge(psg_matches_cl, how='left', left_on=["date", "service"], right_on=["date","service"])
 
     df_d16 = df_d16.drop_duplicates(subset=['date', 'service'], keep='first')
     df_d16.drop(columns=['CA_HT', 'TVA'], inplace=True)
+    df_d16.drop(columns=['Location', 'Home Team','Away Team'], inplace=True)
+    df_d16['Match Happening'].replace(to_replace=0.0,value=1,inplace=True)
+    df_d16['Match Happening-CL'].replace(to_replace=0.0,value=1,inplace=True)
+    df_d16['match_happening'] = df_d16['Match Happening'].copy()
+    df_d16['match_happening_cl'] = df_d16['Match Happening-CL'].copy()
+    df_d16.drop(columns=['Match Happening', 'Match Happening-CL'], inplace=True)
 
     df_d16 = df_d16.reset_index(drop=True)
 
@@ -44,8 +71,6 @@ def merge():
     cyclical_columns()
 
     pass
-
-
 
 if __name__ == '__main__':
     merge()
