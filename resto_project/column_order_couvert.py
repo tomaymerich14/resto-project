dc8b5a17528530ea1ec87dfb3e5256ac12dba39b
Pierre de Metz <demetz.pierre@icloud.com>
2021-09-02 10:28:43 +0200

    d

---
+++ resto_project/column_order_couvert.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import joblib
+
+def column_order_couvert():
+    pipeline_couvert_d2 = joblib.load('../raw_data/model_d2_CO.joblib')
+
+    df2=pd.read_csv('../raw_data/forecasted_services_d2.csv')
+    df2=df2[list(list(pipeline_couvert_d2)[0]._df_columns)]
+    df2.to_csv('../raw_data/forecasted_services_d2.csv', index=False)
+
+    df16=pd.read_csv('../raw_data/forecasted_services_d16.csv')
+    df16=df16[list(list(pipeline_couvert_d2)[0]._df_columns)]
+    df16.to_csv('../raw_data/forecasted_services_d16.csv', index=False)
+
+if __name__=='__main__':
+    column_order_couvert()
