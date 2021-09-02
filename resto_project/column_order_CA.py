c006ab23b32c1f4a2b70ad9eca497cc53c76e66b
Pierre de Metz <demetz.pierre@icloud.com>
2021-09-02 10:56:22 +0200

    order

--- resto_project/column_order_CA.py
+++ resto_project/column_order_CA.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import joblib
 def column_order_CA():
-    pipeline_d2 = joblib.load('../joblibs/model_d2.joblib')
+    pipeline_d2 = joblib.load('../joblibs/model_d2_CA.joblib')
 
     df2=pd.read_csv('../raw_data/forecasted_services_d2.csv')
     df2=df2[list(list(pipeline_d2)[0]._df_columns)]
-    df2.to_csv('../raw_data/forecasted_services_d2.csv', index=False)
+    df2.to_csv('../raw_data/forecasted_services_d2_CA.csv', index=False)
 
     df16=pd.read_csv('../raw_data/forecasted_services_d16.csv')
     df16=df16[list(list(pipeline_d2)[0]._df_columns)]
-    df16.to_csv('../raw_data/forecasted_services_d16.csv', index=False)
+    df16.to_csv('../raw_data/forecasted_services_d16_CA.csv', index=False)
 
 if __name__=='__main__':
     column_order_CA()
--- resto_project/column_order_couvert.py
+++ resto_project/column_order_couvert.py
@@ -6,11 +6,11 @@ def column_order_couvert():
 
     df2=pd.read_csv('../raw_data/forecasted_services_d2.csv')
     df2=df2[list(list(pipeline_couvert_d2)[0]._df_columns)]
-    df2.to_csv('../raw_data/forecasted_services_d2.csv', index=False)
+    df2.to_csv('../raw_data/forecasted_services_d2_CO.csv', index=False)
 
     df16=pd.read_csv('../raw_data/forecasted_services_d16.csv')
     df16=df16[list(list(pipeline_couvert_d2)[0]._df_columns)]
-    df16.to_csv('../raw_data/forecasted_services_d16.csv', index=False)
+    df16.to_csv('../raw_data/forecasted_services_d16_CO.csv', index=False)
 
 if __name__=='__main__':
     column_order_couvert()
